d0dba55d39d9ef9ac1b4515cf6825c7501599cd7
Ricardo Da Silva Familiar <ricada-s@c1r3s4.42porto.com>
2022-11-26 01:57:02 +0000

    ✔

---
+++ C_04/ex03/ft_atoi.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ricada-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 19:56:25 by ricada-s          #+#    #+#             */
+/*   Updated: 2022/10/04 20:13:03 by ricada-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+//#include <stdio.h>
+int	spaces(char *str, int *ptr_i)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while ((str[i] == 43 || str[i] == 45))
+	{
+		if (str[i] == 45)
+			count *= -1;
+		i++;
+	}
+	*ptr_i = i;
+	return (count);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	result;
+	int	i;
+
+	result = 0;
+	sign = spaces(str, &i);
+	while (str[i] && str[i] >= 48 && str[i] <= 57)
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	result *= sign;
+	return (result);
+}
+/*int main(void)
+{
+	char *s = "   -+0312214a507";
+	printf("%d", ft_atoi(s));
+}*/
